522740b2f0455f683a46673c0b5799269c5b7609
pglenn1 <157517894+pglenn1@users.noreply.github.com>
2024-02-07 22:50:35 -0600

    Added border to img; added a rainbow font after completed task; constrained image to 'x' axis; added a visible finish line

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,18 @@
-$( document ).ready(function() {
-   
-  $('#img').click( 
-    function(){
-      //code goes here
-      console.log("position: ", $('#img').position() );
-    }
-  ); 
+$(document).ready(function() {
+  $('#img').click(function() {
+      console.log("position: ", $('#img').position());
+  }); 
   
-  $( "#img" ).draggable({
+  $("#img").draggable({
+      axis: 'x', // Restrict movement to the x-axis
       stop: function() {
-        if(  $('#img').position().left > 300){
-        
-          console.log("user clicked img");
-    $("#outputMsg").text("WOW YOU WIN!");
-    //$("#outputDiv").toggle();
-        }
-    }  
+          if ($('#img').position().left > 500) {
+              console.log("user clicked img");
+              $("#outputMsg").text("Shrek is Free!");
+              $("#outputMsg").toggleClass("rainbow-flash"); // Toggle the rainbow animation class
+          } else {
+              $("#outputMsg").removeClass("rainbow-flash"); // Remove the rainbow animation class if not dragged more than 300 pixels
+          }
+      }  
   });
 });
